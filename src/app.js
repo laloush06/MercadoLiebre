392a270387eb353cbd8abb492d61dfd9d70360f5
laloush06 <laloush95@gmail.com>
2023-11-23 21:41:53 -0300

    probandoRender

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
 const publicPath = path.resolve(__dirname, "../public");
+
 app.use(express.static(publicPath))
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000");
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log("Servidor corriendo en http://localhost:${port}");
 });
 
 app.get("/", (req, res) => {
