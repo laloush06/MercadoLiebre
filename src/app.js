0585c4089e6e4240fca8c1df0549d18e94b6147e
laloush06 <laloush95@gmail.com>
2023-11-28 20:51:37 -0300

    Finalizado

--- src/app.js
+++ src/app.js
@@ -1,23 +1,46 @@
-const express = require("express");
+/* const express = require("express");
 const path = require("path");
 const app = express();
-const publicPath = path.resolve(__dirname, "../public");
 
+const publicPath = path.resolve(__dirname, "../public");
 app.use(express.static(publicPath))
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(3000, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
 });
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
-})
+}) */
 
-app.get("/register", (req, res) =>{
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, "../public");
+
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log (`Servidor corriendo en: http://localhost:${port}`));
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.get("/register", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"))
 });
 
-app.get("/login", (req, res) =>{
+app.get("/login", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
-})
+});
+
+//esto es solo para cumplir con lo visual del mercado liebre
+app.post("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.post("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
